859742bd86bb4391d15f60a6fcdd7290dbcd1bb3
Marc <marcferreresudl@gmail.com>
2025-02-19 11:47:57 +0100

    Error 404 and styles.css

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,33 +1,39 @@
 from django.shortcuts import render
 import urllib.request
 import json
+from http import HTTPStatus
+from urllib.error import HTTPError
 
 # Create your views here.
 
 from django.http import HttpResponse
 
 def index(request):
-    if request.method == 'POST':
-        pokemon = request.POST['pokemon'].lower()
-        pokemon = pokemon.replace(' ', '%20')
-        url_pokeapi = urllib.request.Request(f'https://pokeapi.co/api/v2/pokemon/{pokemon}')
-        url_pokeapi.add_header('User-Agent', 'charmander')
+    try:
+        if request.method == 'POST':
+            pokemon = request.POST['pokemon'].lower()
+            pokemon = pokemon.replace(' ', '%20')
+            url_pokeapi = urllib.request.Request(f'https://pokeapi.co/api/v2/pokemon/{pokemon}')
+            url_pokeapi.add_header('User-Agent', 'charmander')
 
-        source = urllib.request.urlopen(url_pokeapi).read()
+            source = urllib.request.urlopen(url_pokeapi).read()
 
-        list_of_data = json.loads(source)
+            list_of_data = json.loads(source)
 
-        tipos = ", ".join([tipo['type']['name'].capitalize() for tipo in list_of_data['types']])
+            tipos = ", ".join([tipo['type']['name'].capitalize() for tipo in list_of_data['types']])
 
-        data = {
-            "number": str(list_of_data['id']),
-            "name": str(list_of_data['name']).capitalize(),
-            "types": tipos,
-            "sprite": str(list_of_data['sprites']['front_default']),
-        }
+            data = {
+                "number": str(list_of_data['id']),
+                "name": str(list_of_data['name']).capitalize(),
+                "types": tipos,
+                "sprite": str(list_of_data['sprites']['front_default']),
+            }
 
-        print(data)
-    else:
-        data = {}
+            print(data)
+        else:
+            data = {}
 
-    return render(request, 'main/index.html', data)
+        return render(request, 'main/index.html', data)
+    except HTTPError as e:
+        if e.code == 404:
+            return render(request, 'main/404.html')
