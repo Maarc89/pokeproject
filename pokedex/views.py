58db6dbb7700978389da9098196bd4f854b911c6
Marc <marcferreresudl@gmail.com>
2025-02-19 11:27:29 +0100

    Basic structure

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
+import urllib.request
+import json
 
 # Create your views here.
 
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("¡Bienvenido a la Pokédex!")
+def index(request):
+    if request.method == 'POST':
+        pokemon = request.POST['pokemon'].lower()
+        pokemon = pokemon.replace(' ', '%20')
+        url_pokeapi = urllib.request.Request(f'https://pokeapi.co/api/v2/pokemon/{pokemon}')
+        url_pokeapi.add_header('User-Agent', 'charmander')
+
+        source = urllib.request.urlopen(url_pokeapi).read()
+
+        list_of_data = json.loads(source)
+
+        tipos = ", ".join([tipo['type']['name'].capitalize() for tipo in list_of_data['types']])
+
+        data = {
+            "number": str(list_of_data['id']),
+            "name": str(list_of_data['name']).capitalize(),
+            "types": tipos,
+            "sprite": str(list_of_data['sprites']['front_default']),
+        }
+
+        print(data)
+    else:
+        data = {}
+
+    return render(request, 'main/index.html', data)
