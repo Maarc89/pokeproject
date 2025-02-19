f9f3cc1ea50f3b364f7e243658fc19c04fa185bb
Marc <marcferreresudl@gmail.com>
2025-02-19 19:55:18 +0100

    Postgresql done

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Pokemon(models.Model):
+    number = models.IntegerField(unique=True)
+    name = models.CharField(max_length=100, unique=True)
+    types = models.CharField(max_length=255)
+    abilities = models.CharField(max_length=255)
+    sprite = models.URLField()
+
+    def __str__(self):
+        return self.name
